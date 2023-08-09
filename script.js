81591f3903b454839417b317ba9784c2f073b4ff
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-08-09 02:01:44 +0100

    added randomization to squares

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+let colors = generateRandomColor(6)
+
 
 let squares = document.querySelectorAll(".square")
 let pickedColor =pickColor()
 let colorDisplay = document.getElementById("colorDisplay")
 let message = document.querySelector('#message')
+let h1 = document.querySelector("h1");
+
 
 colorDisplay.textContent=pickedColor
 
@@ -22,6 +18,7 @@ for(let i =0; i<squares.length; i++){
         if(clickedColor===pickedColor){
             message.textContent= "Correct!"
             changeColor(clickedColor)
+            h1.style.backgroundColor = clickedColor
         }
         else{
             this.style.backgroundColor = "#232323"
@@ -37,7 +34,24 @@ function changeColor(color){
 }
 
 function pickColor(){
-    var random = Math.floor(Math.random() *colors.length)
+    var random = Math.floor(Math.random() * colors.length)
     return colors[random]
     
 }
+function generateRandomColor(num){
+    var arr = []
+    for (let i=0; i<num; i++){
+        arr.push(randomColor())
+    }
+    return arr
+    
+}
+
+function randomColor (){
+    let r =Math.floor(Math.random()* 256)
+    let g =Math.floor(Math.random()* 256)
+    let b =Math.floor(Math.random()* 256)
+    return "rgb("+ r +", " + g + ", "+ b +")"
+}
+
+
